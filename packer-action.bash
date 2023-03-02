81ba3936a79de84b3ac1b506007e1c167c5fd870
Sibin Grasic <sibin.grasic@oblak.studio>
2023-03-02 17:39:44 +0100

    fix: Fixed copy includes

--- packer-action.bash
+++ packer-action.bash
@@ -1,14 +1,23 @@
 #!/bin/bash
 set -e
 
-NEXT_VERSION=$(npm run semantic-release --ci=false --dry-run | grep -oP 'Published release \K.*? ')
-CURRENT_VERSION=$(< "$ACTION_VERSION_FILE" grep Version | head -1 | awk -F= "{ print $2 }" | sed 's/[Version:,\",]//g' | tr -d ':space:')
+NEXT_VERSION=$(npm run semantic-release -- --ci=false --dry-run | grep -oP 'Published release \K.*? ')
+CURRENT_VERSION=$(< "$ACTION_VERSION_FILE" grep "Version" | head -1 | awk -F= "{ print $2 }" | sed 's/[* Version:,\",]//g' | tr -d ':space:')
 
 sed -i "s/$CURRENT_VERSION/$NEXT_VERSION/g" "$ACTION_VERSION_FILE"
 
-mkdir "/tmp/$ACTION_PLUGIN_SLUG"
+rm -rf "/tmp/$ACTION_PLUGIN_SLUG" 2>/dev/null
+mkdir -p "/tmp/$ACTION_PLUGIN_SLUG"
 
-cp -ar "$ACTION_INCLUDE_FILES" "/tmp/$ACTION_PLUGIN_SLUG" 2>/dev/null
+if [ -n "$ACTION_INCLUDE_FILES" ];
+then
+  echo "Copying files to plugin directory"
+  IFS=' ' read -r -a folders <<< "$ACTION_INCLUDE_FILES"
+
+  cp -ar ./*.php "/tmp/$ACTION_PLUGIN_SLUG"
+  cp -ar -- "${folders[@]}" "/tmp/$ACTION_PLUGIN_SLUG"
+fi
+
 cd /tmp
 zip -qr "$ACTION_OUTPUT_FILE" "$ACTION_PLUGIN_SLUG"
 echo "name=output_file::${ACTION_OUTPUT_FILE}" >> "$GITHUB_OUTPUT"
